48de72846c1a3e1fbc9c2d78057fdbbf73a76887
José Luis Aguilar Sánchez <153824159+joselaguilarhz@users.noreply.github.com>
2025-09-29 12:27:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // La base será la raíz ya que usaremos el dominio personalizado
+  base: './', // 👈 rutas relativas para GitHub Pages
 })
