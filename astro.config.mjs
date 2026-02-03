80d3a50e6336ded434e467218105e329a817a6ef
José Luis Aguilar Sánchez <153824159+joselaguilarhz@users.noreply.github.com>
2026-02-03 12:15:51 +0100

    Add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://joselaguilarhz.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
